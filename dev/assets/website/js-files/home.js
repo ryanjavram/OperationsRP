6bb56cabbaeb564a98dedd1cf622efdf51c0b2a6
ryanjavram <56175885+ryanjavram@users.noreply.github.com>
2022-02-04 22:26:37 -0500

    Update home.js

--- dev/assets/website/js-files/home.js
+++ dev/assets/website/js-files/home.js
@@ -1,12 +1,1 @@
-window.addEventListener("load", function() {
-    const form = document.getElementById('my-form');
-    form.addEventListener("submit", function(e) {
-      e.preventDefault();
-      const data = new FormData(form);
-      const action = e.target.action;
-      fetch(action, {
-        method: 'POST',
-        body: data,
-      })
-      .then(() =>
-        function addNewClass(idd){   idd.className="newClass"; }
+EDIT
