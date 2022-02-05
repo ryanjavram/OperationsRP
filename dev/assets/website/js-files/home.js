114bd984e10025e6fc80046ecd69b2bbbcf31ebe
ryanjavram <56175885+ryanjavram@users.noreply.github.com>
2022-02-04 21:18:47 -0500

    Update home.js

--- dev/assets/website/js-files/home.js
+++ dev/assets/website/js-files/home.js
@@ -1,1 +1,15 @@
-EDIT
+window.addEventListener("load", function() {
+    const form = document.getElementById('my-form');
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const data = new FormData(form);
+      const action = e.target.action;
+      fetch(action, {
+        method: 'POST',
+        body: data,
+      })
+      .then(() => {
+        alert("Success!");
+      })
+    });
+  });
